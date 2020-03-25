1bffcb1aac65717c45b8acc10e0aae9a24689aa1
Jeff Lay <phritolay@icloud.com>
2020-03-25 13:44:41 -0400

    add sass, stylelint and prettier

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+// TODO: Remove next-js-welcome-page.scss
+import "../styles/next-js-welcome-page.scss"
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
