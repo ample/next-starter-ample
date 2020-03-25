05b04e3cb49dbe2268dd7dd6a160835fb8c985a1
Jeff Lay <phritolay@icloud.com>
2020-03-25 16:00:31 -0400

    add eslint and js files

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
+import React from "react"
+
 import "../styles/libs/sanitize.css"
 
 // TODO: Remove next-js-welcome-page.scss
 import "../styles/next-js-welcome-page.scss"
 
+// eslint-disable-next-line react/prop-types
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
