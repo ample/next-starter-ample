c3825a2135b513b28fac2257f33028c254aa85e4
Jeff Lay <phritolay@icloud.com>
2021-11-01 11:55:55 -0400

    update eslint config to lastest on other projects

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
   env: {
-    es6: true,
     browser: true,
-    node: true,
-    jest: true
+    es6: true,
+    jest: true,
+    node: true
   },
-  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:jsx-a11y/recommended"],
+  extends: ["eslint:recommended", "plugin:jsx-a11y/recommended", "plugin:react/recommended"],
   globals: {
     __PATH_PREFIX__: true
   },
@@ -23,14 +23,18 @@ module.exports = {
   ],
   parser: "@babel/eslint-parser",
   parserOptions: {
+    babelOptions: {
+      presets: ["@babel/preset-react"]
+    },
     ecmaFeatures: {
       jsx: true
-    }
+    },
+    ecmaVersion: es2021,
+    requireConfigFile: false,
+    sourceType: "module"
   },
-  plugins: ["prettier", "jsx-a11y"],
+  plugins: ["jest", "jsx-a11y", "prettier", "react", "sort-keys-fix"],
   rules: {
-    "react/no-unescaped-entities": "off",
-    "react/prop-types": [2, { ignore: ["className"] }],
     "jsx-a11y/anchor-is-valid": [
       "error",
       {
@@ -38,7 +42,12 @@ module.exports = {
         specialLink: ["hrefLeft", "hrefRight"],
         aspects: ["invalidHref", "preferButton"]
       }
-    ]
+    ],
+    "import/no-unresolved": 0,
+    "react/no-unescaped-entities": 0,
+    "react/prop-types": [2, { ignore: ["className"] }],
+    "sort-keys": ["error", "asc", { caseSensitive: true, minKeys: 3, natural: true }],
+    "sort-keys-fix/sort-keys-fix": 1
   },
   settings: {
     react: {
