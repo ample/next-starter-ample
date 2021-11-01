8e607f9abeb33683b1a94077e6c29d7404263870
Jeff Lay <phritolay@icloud.com>
2021-11-01 11:36:18 -0400

    add postcssOptions to next.config.js
    to override next's postcss defaults

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 const path = require("path")
+const postcssPlugins = require("./postcss.config")
+
+// ---------------------------------------------------------
 
 module.exports = {
+  requireConfigFile: false,
   publicRuntimeConfig: {
     DEBUG_MEDIA_QUERIES: process.env.DEBUG_MEDIA_QUERIES
+  },
+  postcssOptions: {
+    postcssPlugins
   },
   sassOptions: {
     includePaths: [path.join(__dirname, "src/styles")],
