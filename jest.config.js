980e31b02c96b1a8f27236a28af81b41e04b37e9
Jeff Lay <phritolay@users.noreply.github.com>
2022-03-23 22:08:48 -0400

    update jest config to use next/jest

--- jest.config.js
+++ jest.config.js
@@ -1,30 +1,20 @@
-module.exports = {
-  collectCoverageFrom: [
-    '**/*.{js,jsx,ts,tsx}',
-    '!**/*.d.ts',
-    '!**/node_modules/**',
-  ],
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
   moduleNameMapper: {
     '\\@components\\/(.*)$': `<rootDir>/components/$1`,
     '\\@content\\/(.*)$': `<rootDir>/content/$1`,
     '\\@layout\\/(.*)$': `<rootDir>/layout/$1`,
     '\\@root\\/(.*)$': `<rootDir>/$1`,
     '\\@templates\\/(.*)$': `<rootDir>/templates/$1`,
-    '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$':
-      '<rootDir>/.jest/__mocks__/file.js',
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
   },
   setupFilesAfterEnv: ['<rootDir>/.jest/jest.setup.js'],
-  testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': [
-      '<rootDir>/node_modules/babel-jest',
-      { configFile: './.jest/.babelrc' },
-    ],
-  },
-  transformIgnorePatterns: [
-    '/node_modules/',
-    '^.+\\.module\\.(css|sass|scss)$',
-  ],
+  testEnvironment: 'jest-environment-jsdom',
 };
+
+module.exports = createJestConfig(customJestConfig);
